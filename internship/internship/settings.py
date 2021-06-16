f11e6b16a390a83edce6e2b90348e107ab896451
Manthan171 <mmparmar17122000@gmail.com>
2021-06-16 08:42:04 +0530

    no changes

--- internship/internship/settings.py
+++ internship/internship/settings.py
@@ -188,5 +188,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mmparmar17122000@gmail.com'
-EMAIL_HOST_PASSWORD = 'milu@1712'
+EMAIL_HOST_PASSWORD = '******'
 
